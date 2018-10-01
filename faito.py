c28af018861c5e3ec71afd5fec95c98400d9be64
Will Walker <wewalker@berkeley.edu>
2018-10-01 00:44:44 -0700

    add prints

--- faito.py
+++ faito.py
@@ -1,8 +1,16 @@
 import random
+import testo
 
 def fight(p, e):
 
+	turn = 0
+	print("Starting health:")
+
 	while True:
+
+		print(str(p.n) + ": " + str(p.h) + "\n" + str(e.n) + ": " + str(e.h) + "\n")
+
+		print("Turn " + str(turn) + ":")
 
 		# Roll to determine who goes first
 		proll = p.s + random.randint(1, 6)
@@ -20,15 +28,32 @@ def fight(p, e):
 
 		# First fighter attacks
 		fcrit = random.randint(0, 100) < first.l
+		print(first.n + (" crits!" if fcrit else " attacks."))
 		second.h -= 2 * first.a - second.d if fcrit else first.a - second.d
 		if second.h <= 0:
+			second.h = 0
 			break
 
 		# Second fighter attacks
 		scrit = random.randint(0, 100) < second.l
+		print(second.n + (" crits!" if fcrit else " attacks."))
 		first.h -= 2 * second.a - first.d if scrit else second.a - first.d
 		if first.h <= 0:
+			first.h = 0
 			break
 
+		turn += 1
+
+	# Print final health values.
+	print(str(p.n) + ": " + str(p.h) + "\n" + str(e.n) + ": " + str(e.h) + "\n")
+	print(p.n + " wins!" if p.h > 0 else e.n + " wins!")
 	return p.h > 0
 
+player = testo.Player(10, 2, 1, 5, 50, "Buton")
+enemy = testo.Enemy(10, 1, 1, 5, 50, "Sam")
+
+print()
+
+fight(player, enemy)
+
+print()
