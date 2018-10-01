0186f49fb7634e303da4723eeb4b3023ad78b856
ButonLiam <burtonlian99@gmail.com>
2018-09-30 23:26:01 -0700

    Patch 0.0.4
    Added:
    - textBox Function

--- testo.py
+++ testo.py
@@ -1,13 +1,40 @@
-class Silly:
-    def __init__(self, health, attack, defense):
+import random, time
+
+class Enemy:
+    def __init__(self, health, attack, defense, speed, luck, name):
         self.h = health
         self.a = attack
         self.d = defense
+        self.s = speed
+        self.l = luck
+        self.n = name
 
     def __str__(self):
-    	return "Silly: Attack = " + str(self.a) + " Defense = " + str(self.d) + " Health = " + str(self.h)
+        return str(self.n) + ": Health = " + str(self.h) + " Attack = " + str(self.a) + " Defense = " + str(self.d) +  " Speed = " + str(self.s) + " Luck = " + str(self.l)
 
-        
-goblin = Silly(1, 1, 1)
-print(goblin)
+class Player:
+    def __init__(self, health, attack, defense, speed, luck, name):
+        self.h = health
+        self.a = attack
+        self.d = defense
+        self.s = speed
+        self.l = luck
+        self.n = name
+
+    def __str__(self):
+        return str(self.n) + ":\n\tHealth = " + str(self.h) + "\n\tAttack = " + str(self.a) + "\n\tDefense = " + str(self.d) +  "\n\tSpeed = " + str(self.s) + "\n\tLuck = " + str(self.l)
+
+player = Player(10, 10, 10, 10, 10, "Will")
+print(player)
+
+def textBox(text):
+    for i in text:
+        print(i, end='')
+        time.sleep(.01)
+    print()
+
+
+
+
+    
 
