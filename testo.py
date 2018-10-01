f23e08ddb4f4e9c59d55062e767fa509ed1149ab
Will Walker <wewalker@berkeley.edu>
2018-09-30 19:44:18 -0700

    Add str method for Silly

--- testo.py
+++ testo.py
@@ -4,6 +4,10 @@ class Silly:
         self.a = attack
         self.d = defense
 
+    def __str__(self):
+    	return "Silly: Attack = " + str(self.a) + " Defense = " + str(self.d) + " Health = " + str(self.h)
+
         
 goblin = Silly(1, 1, 1)
 print(goblin)
+
