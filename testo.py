90110e23cb712a36905d79fa88af9f114c9fd4eb
ButonLiam <burtonlian99@gmail.com>
2018-09-30 19:33:42 -0700

    Patch 0.0.3: Testo

---
+++ testo.py
@@ -0,0 +1,9 @@
+class Silly:
+    def __init__(self, health, attack, defense):
+        self.h = health
+        self.a = attack
+        self.d = defense
+
+        
+goblin = Silly(1, 1, 1)
+print(goblin)
