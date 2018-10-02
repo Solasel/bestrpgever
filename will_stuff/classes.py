7ee6c9e44c77681538ad4eefed7eae78dafedd31
Will Walker <wewalker@berkeley.edu>
2018-10-02 16:17:40 -0700

    Update stuff

--- will_stuff/classes.py
+++ will_stuff/classes.py
@@ -12,3 +12,14 @@ class Fighter:
 	def __str__(self):
 		return str(self.name) + ":\n\tHealth = " + str(self.health) + "\n\tAttack = " + str(self.attack) + "\n\tDefense = " + str(self.defense) +  "\n\tSpeed = " + str(self.speed) + "\n\tLuck = " + str(self.luck)
 
+class Place:
+	def __init__(self, n, d, t, i, m):
+		self.name = n
+		self.desc = d
+		self.type = t
+		self.item = i
+		self.maplink = m
+
+	def __str__(self):
+		return self.name + ": " + self.type + "\n" + self.desc
+
--- will_stuff/faito.py
+++ will_stuff/faito.py
@@ -62,3 +62,4 @@ print()
 fight(player, enemy)
 
 print()
+
