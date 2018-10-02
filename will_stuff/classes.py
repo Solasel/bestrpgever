4bdd1087f5b13f45205862ef058d6fb37cd3b7ba
Will Walker <wewalker@berkeley.edu>
2018-10-02 16:35:41 -0700

    Add testing for places

--- will_stuff/classes.py
+++ will_stuff/classes.py
@@ -13,13 +13,11 @@ class Fighter:
 		return str(self.name) + ":\n\tHealth = " + str(self.health) + "\n\tAttack = " + str(self.attack) + "\n\tDefense = " + str(self.defense) +  "\n\tSpeed = " + str(self.speed) + "\n\tLuck = " + str(self.luck)
 
 class Place:
-	def __init__(self, n, d, t, i, m):
+	def __init__(self, n, d, t):
 		self.name = n
 		self.desc = d
 		self.type = t
-		self.item = i
-		self.maplink = m
 
 	def __str__(self):
-		return self.name + ": " + self.type + "\n" + self.desc
+		return self.name + ": " + self.type + "\n- " + self.desc
 
---
+++ will_stuff/placetest.py
@@ -0,0 +1,6 @@
+import classes
+
+loc = classes.Place("Havenshire", "A small hamlet in Westshire.", "Town")
+
+print(loc)
+
