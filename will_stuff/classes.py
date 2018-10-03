3388fc573c372e26bfb05ec4f08750d388d283a3
Will Walker <wewalker@berkeley.edu>
2018-10-02 17:47:52 -0700

    Add methods to fighter

--- will_stuff/classes.py
+++ will_stuff/classes.py
@@ -1,3 +1,5 @@
+import random
+
 class Fighter:
 	def __init__(self, h, a, d, s, l, n):
 		self.health = h
@@ -7,8 +9,28 @@ class Fighter:
 		self.luck = l
 		self.name = n
 
+		self.money = 0
+
 	def __str__(self):
 		return str(self.name) + ":\n\tHealth = " + str(self.health) + "\n\tAttack = " + str(self.attack) + "\n\tDefense = " + str(self.defense) +  "\n\tSpeed = " + str(self.speed) + "\n\tLuck = " + str(self.luck)
+
+	def deposit(self, amount):
+		self.money += amount
+	
+	def withdraw(self, amount):
+		self.money -= amount
+
+	def gamble(self, amount, guess):
+		print("You are betting " + str(amount) + " doodles on the roll of a 6-die.")
+		print("Your guess is " + str(guess) + ". If you win, you receive double the money. If you lose, you lose it all!\n")
+
+		if random.randint(1, 6) == guess:
+			print("You win!")
+			self.deposit(amount)
+			return
+		print("You lose!")
+		self.withdraw(amount)
+		return
 
 class Place:
 	def __init__(self, n, d, t):
